c5ceec8795d62642a15973cc61a7383b7c327ea8
patelheta <patelheta385@gmail.com>
2023-02-14 16:22:56 -0800

    integration of Alan AI in the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import NewsCards from './components/NewsCards/NewsCards';
+
+const alanKey = '7fecfc3cfa0ea92f2d59ca3f954c770e2e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 function App() {
+
+  const [newsArticles, setNewsArticles] = useState([]);
+
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command, articles }) => {
+        if (command === 'newHeadlines') {
+          console.log(articles);
+          setNewsArticles(articles);
+        }
+      }
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <h1>Alan AI News Application</h1>
+      <NewsCards articles={newsArticles} />
     </div>
   );
 }
